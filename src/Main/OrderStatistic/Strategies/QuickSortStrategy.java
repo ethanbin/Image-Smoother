6b1618eec6c9ee823c87e8986ba2fd948c18f081
Ethan <ethanbinya@aol.com>
2018-12-09 00:16:49 -0500

    Implemented quick sort strategy's findColor method

--- src/Main/OrderStatistic/Strategies/QuickSortStrategy.java
+++ src/Main/OrderStatistic/Strategies/QuickSortStrategy.java
@@ -30,6 +30,34 @@ public class QuickSortStrategy extends OrderStatisticStrategy {
             throw new ArrayIndexOutOfBoundsException("Target value cannot be negative - user requested the " +
                     Integer.toString(k)  + "th smallest number.");
 
-        return null;
+        quickSort(arr, 0, arr.length-1);
+        return arr[k];
+    }
+
+    private int partition(Color [] arr, int start, int end){
+        Color pivotColor = arr[end];
+        int pivotGreyLevelValue = (pivotColor.getRed() + pivotColor.getGreen() + pivotColor.getBlue()) / 3;
+        int j = start;
+        for (int i = start; i < end; i++){
+            Color currentColor = arr[i];
+            int currentGreyLevel = (currentColor.getRed() + currentColor.getGreen() + currentColor.getBlue()) / 3;
+            if (currentGreyLevel < pivotGreyLevelValue) {
+                arr[i] = arr[j];
+                arr[j] = currentColor;
+                j++;
+            }
+        }
+        // swap pivot with first element greater than pivot
+        arr[end] = arr[j];
+        arr[j] = pivotColor;
+        return j;
+    }
+
+    private void quickSort(Color[] arr, int start, int end){
+        if (start < end){
+            int pivot = partition(arr, start, end);
+            quickSort(arr, start, pivot-1);
+            quickSort(arr,pivot+1, end);
+        }
     }
 }
